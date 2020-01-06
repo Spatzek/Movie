0c5c49d8a0cc6339704aa920eaae7018643fa841
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2020-01-06 11:50:56 +0100

    Revert "setup gui"
    This reverts commit 7829e919335ab2636045c3259f63b63e5a471b7e.

--- src/moviecollector/MovieCollector.java
+++ src/moviecollector/MovieCollector.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package moviecollector.gui;
+package moviecollector;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +19,7 @@ public class MovieCollector extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/MovieCollectorView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
